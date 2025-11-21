dfc734a076b5a602bcfb983d1d7d288c954c66b4
uditsharma725 <uditsharma1250@gmail.com>
2025-11-21 14:08:25 +0530

    database setup

--- src/main/java/in/lekhai/LekhaiApplication.java
+++ src/main/java/in/lekhai/LekhaiApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class LekhaiApplication {
 
-	static void main(String[] args) {
+	public static void main(String[] args) {
 		SpringApplication.run(LekhaiApplication.class, args);
 	}
 
