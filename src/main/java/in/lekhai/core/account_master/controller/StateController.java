6cbe53beaece880329f6d07a527e09ed16e917db
uditsharma725 <uditsharma1250@gmail.com>
2026-02-15 21:33:24 +0530

    updated STATE

--- src/main/java/in/lekhai/core/account_master/controller/StateController.java
+++ src/main/java/in/lekhai/core/account_master/controller/StateController.java
@@ -1,9 +1,13 @@
 package in.lekhai.core.account_master.controller;
 
+import in.lekhai.accountmaster.state.api.StateApi;
+import in.lekhai.accountmaster.state.dto.StateResponse;
 import in.lekhai.authentication.utils.SecurityExpressions;
 import in.lekhai.common.Result;
-import in.lekhai.core.account_master.dto.StateResponse;
 import in.lekhai.core.account_master.service.StateService;
+import in.lekhai.shop.context.model.ShopContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,19 +17,23 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/state")
 @PreAuthorize(SecurityExpressions.IS_SHOP_OWNER)
-public class StateController {
+public class StateController implements StateApi {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final StateService stateService;
 
-    public StateController(StateService stateService) {
+    public StateController(
+            StateService stateService
+    ) {
         this.stateService = stateService;
     }
 
-    @GetMapping("/list-all")
-    public ResponseEntity<Result<List<StateResponse>>> listAllStates() {
+    @Override
+    public ResponseEntity<List<StateResponse>> listAllStates() {
+        log.info("Got a request to list all states {}", ShopContext.getShopCode());
         List<StateResponse> response = stateService.listStates();
-        return ResponseEntity.ok(Result.success(response));
+        log.info("Successfully listed all states {}", ShopContext.getShopCode());
+        return ResponseEntity.ok(response);
     }
 }
