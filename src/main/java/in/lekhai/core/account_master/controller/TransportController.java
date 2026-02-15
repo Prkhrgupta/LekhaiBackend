b41ac43303c47e102d4589c81e141e9458a2253c
uditsharma725 <uditsharma1250@gmail.com>
2026-02-15 21:42:22 +0530

    updated TRANSPORT

--- src/main/java/in/lekhai/core/account_master/controller/TransportController.java
+++ src/main/java/in/lekhai/core/account_master/controller/TransportController.java
@@ -1,11 +1,15 @@
 package in.lekhai.core.account_master.controller;
 
+import in.lekhai.accountmaster.transport.api.TransportApi;
+import in.lekhai.accountmaster.transport.dto.TransportRequest;
+import in.lekhai.accountmaster.transport.dto.TransportResponse;
 import in.lekhai.authentication.utils.SecurityExpressions;
 import in.lekhai.common.Result;
-import in.lekhai.core.account_master.dto.TransportRequest;
-import in.lekhai.core.account_master.dto.TransportResponse;
 import in.lekhai.core.account_master.service.TransportService;
+import in.lekhai.shop.context.model.ShopContext;
 import jakarta.validation.Valid;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
@@ -13,25 +17,31 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/transport")
 @PreAuthorize(SecurityExpressions.IS_SHOP_OWNER)
-public class TransportController {
+public class TransportController implements TransportApi {
 
+    private final Logger log = LoggerFactory.getLogger(this.getClass());
     private final TransportService transportService;
 
-    public TransportController(TransportService transportService) {
+    public TransportController(
+            TransportService transportService
+    ) {
         this.transportService = transportService;
     }
 
-    @PostMapping("/create")
-    public ResponseEntity<Result<TransportResponse>> createTransport(@RequestBody @Valid TransportRequest request) {
+    @Override
+    public ResponseEntity<TransportResponse> createTransport(@Valid TransportRequest request) {
+        log.info("Got a request to create a new transport {} :: {}", ShopContext.getShopCode(), request.toString());
         TransportResponse response = transportService.createTransport(request);
-        return ResponseEntity.ok(Result.success(response));
+        log.info("Successfully added transport details {} :: {}", ShopContext.getShopCode(), response.toString());
+        return ResponseEntity.ok(response);
     }
 
-    @GetMapping("/list-all")
-    public ResponseEntity<Result<List<TransportResponse>>> listTransports() {
+    @Override
+    public ResponseEntity<List<TransportResponse>> listTransports() {
+        log.info("Got a request to list all transport detail {}", ShopContext.getShopCode());
         List<TransportResponse> response = transportService.listTransports();
-        return ResponseEntity.ok(Result.success(response));
+        log.info("Successfully listed all transport details {}", ShopContext.getShopCode());
+        return ResponseEntity.ok(response);
     }
 }
--- src/main/java/in/lekhai/core/account_master/dto/TransportRequest.java
+++
@@ -1,9 +0,0 @@
-package in.lekhai.core.account_master.dto;
-
-import jakarta.validation.constraints.NotBlank;
-
-public record TransportRequest(
-        @NotBlank(message = "name is required") String name,
-        String phone,
-        String gstNo) {
-}
