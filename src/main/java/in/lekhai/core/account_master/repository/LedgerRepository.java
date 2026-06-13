61682134d17a38488bdf568606d801a962cad940
prakhar gupta <prakhar.gu01@gmail.com>
2026-06-13 13:59:07 +0530

    Initial pagination working

--- src/main/java/in/lekhai/core/account_master/repository/LedgerRepository.java
+++ src/main/java/in/lekhai/core/account_master/repository/LedgerRepository.java
@@ -1,6 +1,8 @@
 package in.lekhai.core.account_master.repository;
 
 import in.lekhai.core.account_master.domain.Ledger;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,4 +11,5 @@ import java.util.Optional;
 @Repository
 public interface LedgerRepository extends CrudRepository<Ledger, Long> {
     Optional<Ledger> findByGstInNumber(String gstInNumber);
+    Page<Ledger> findAll(Pageable pageable);
 }
