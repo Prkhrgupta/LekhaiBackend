a4bc5d73644f26c7f274a2e0c7e271459e9e95c9
prakhar gupta <prakhar.gu01@gmail.com>
2026-06-24 11:53:38 +0530

    add account group filter for ledger dropdown

--- src/main/java/in/lekhai/core/account_master/repository/LedgerRepository.java
+++ src/main/java/in/lekhai/core/account_master/repository/LedgerRepository.java
@@ -4,7 +4,7 @@ import in.lekhai.core.account_master.domain.Ledger;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jdbc.repository.query.Query;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
@@ -12,7 +12,7 @@ import java.util.List;
 import java.util.Optional;
 
 @Repository
-public interface LedgerRepository extends CrudRepository<Ledger, Long> {
+public interface LedgerRepository extends ListCrudRepository<Ledger, Long> {
     Optional<Ledger> findByGstInNumber(String gstInNumber);
     Page<Ledger> findAll(Pageable pageable);
 
