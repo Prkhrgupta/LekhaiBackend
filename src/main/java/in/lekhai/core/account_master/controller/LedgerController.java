68fdfbcd71358f4ebf57ef47477348d95c092d9f
Gupta <prakhar.gu01@gmail.com>
2026-07-26 06:02:18 -0700

    fix compile issue

--- src/main/java/in/lekhai/core/account_master/controller/LedgerController.java
+++ src/main/java/in/lekhai/core/account_master/controller/LedgerController.java
@@ -45,6 +45,12 @@ public class LedgerController implements LedgerApi {
         return ResponseEntity.ok(response);
     }
 
+    // remove this then voucher-impl is merged
+    @Override
+    public ResponseEntity<LedgerBalanceResponse> getLedgerBalance(@NotNull Long aLong) {
+        return null;
+    }
+
     @Override
     public ResponseEntity<LedgerResponse> getLedgerByGstin(@NotNull @Valid String gstIn) {
         LedgerResponse ledgerResponseByGstIn = ledgerService.getLedgerResponseByGstIn(gstIn);
